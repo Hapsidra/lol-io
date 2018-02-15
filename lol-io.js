e4904988425914febc1a4d37d2567683569e4e7f
Максим Ефимов <hapsidra@iMac-LocalAdmin.local>
2018-02-15 16:44:50 +0900

    initial commit

---
+++ lol-io.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+
+const inputPath = 'input.txt';
+const outputPath = 'output.txt';
+
+fs.writeFileSync(outputPath, '');
+
+var index = 0;
+var lines;
+if (fs.existsSync(inputPath)) {
+    lines = fs.readFileSync('input.txt', 'utf8').split('\n');
+}
+else {
+    lines = [];
+    fs.open(inputPath);
+}
+
+exports.readline = function() {
+    return lines[index++];
+}
+
+exports.print = function(){
+    var result = '';
+    for(var i = 0; i < arguments.length; i++){
+        result += arguments[i] + ' ';
+    }
+    fs.appendFileSync(outputPath, result + '\n', 'utf8');
+}
